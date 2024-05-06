27b2c23750d83704bbb8cad7cea6e49701c2e2d2
Jeff L <jeff@langrsoft.com>
2024-05-06 15:21:31 -0600

    6

--- src/test/java/com/langrsoft/domain/AnAddressRetriever.java
+++ src/test/java/com/langrsoft/domain/AnAddressRetriever.java
@@ -1,81 +1,78 @@
 package com.langrsoft.domain;
 
+import com.langrsoft.util.Http;
+import com.langrsoft.util.HttpImpl;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
-import com.langrsoft.util.*;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 // START:test
+import static org.mockito.AdditionalMatchers.and;
 // ...
-// START_HIGHLIGHT
+// END:test
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-// END_HIGHLIGHT
 
 class AnAddressRetriever {
-    // START_HIGHLIGHT
-    Http http = mock(Http.class);
-    // END_HIGHLIGHT
+   Http http = mock(Http.class);
 
-    @Test
-    void answersAppropriateAddressForValidCoordinates() {
-        // START_HIGHLIGHT
-        when(http.get(contains("lat=38.000000&lon=-104.000000"))).thenReturn(
-        // END_HIGHLIGHT
+   // START:test
+   @Test
+   void answersAppropriateAddressForValidCoordinates() {
+      when(http.get(
+         // START_HIGHLIGHT
+         and(contains("lat=38.000000"), contains("lon=-104.000000"))))
+         // END_HIGHLIGHT
+         .thenReturn(
+            // ...
+            // END:test
             """
-                {"address":{
-                  "house_number":"324",
-                  "road":"Main St",
-                  "city":"Anywhere",
-                  "state":"Colorado",
-                  "postcode":"81234",
-                  "country_code":"us"}}
-                """);
-        var retriever = new AddressRetriever(http);
+               {"address":{
+                 "house_number":"324",
+                 "road":"Main St",
+                 "city":"Anywhere",
+                 "state":"Colorado",
+                 "postcode":"81234",
+                 "country_code":"us"}}
+               """);
+      var retriever = new AddressRetriever(http);
 
-        var address = retriever.retrieve(38, -104);
-        // ...
-        // END:test
+      var address = retriever.retrieve(38, -104);
 
-        assertEquals("324", address.house_number());
-        assertEquals("Main St", address.road());
-        assertEquals("Anywhere", address.city());
-        assertEquals("Colorado", address.state());
-        assertEquals("81234", address.postcode());
-        // START:test
-    }
-    // ...
-// END:test
+      assertEquals("324", address.house_number());
+      assertEquals("Main St", address.road());
+      assertEquals("Anywhere", address.city());
+      assertEquals("Colorado", address.state());
+      assertEquals("81234", address.postcode());
+      // START:test
+   }
+   // END:test
 
-    // START:throws
-    @Test
-    void throwsWhenNotUSCountryCode() {
-        // START_HIGHLIGHT
-        when(http.get(anyString())).thenReturn("""
-            {"address":{ "country_code":"not us"}}""");
-       // END_HIGHLIGHT
-        var retriever = new AddressRetriever(http);
+   @Test
+   void throwsWhenNotUSCountryCode() {
+      when(http.get(anyString())).thenReturn("""
+         {"address":{ "country_code":"not us"}}""");
+      var retriever = new AddressRetriever(http);
 
-        assertThrows(UnsupportedOperationException.class,
-            () -> retriever.retrieve(1.0, -1.0));
-    }
-    // END:throws
+      assertThrows(UnsupportedOperationException.class,
+         () -> retriever.retrieve(1.0, -1.0));
+   }
 
-    @Disabled("works as of 2024-Mar-24")
-    @Tag("slow")
-    @Test
-    void liveIntegrationTest() {
-        var retriever = new AddressRetriever(new HttpImpl());
+   @Disabled("works as of 2024-Mar-24")
+   @Tag("slow")
+   @Test
+   void liveIntegrationTest() {
+      var retriever = new AddressRetriever(new HttpImpl());
 
-        var address = retriever.retrieve(38.8372956, -104.8255679);
+      var address = retriever.retrieve(38.8372956, -104.8255679);
 
-        assertEquals("North Cascade Avenue", address.road());
-        assertEquals("Colorado Springs", address.city());
-        assertEquals("Colorado", address.state());
-        assertEquals("80903", address.postcode());
-    }
-    // START:test
+      assertEquals("North Cascade Avenue", address.road());
+      assertEquals("Colorado Springs", address.city());
+      assertEquals("Colorado", address.state());
+      assertEquals("80903", address.postcode());
+   }
 }
-// END:test
